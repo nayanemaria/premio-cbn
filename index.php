a14a67a134d1f9938c0966799482a3ff05e6bd47
nayanemaria <nayane.maria.543@gmail.com>
2021-02-03 19:28:26 -0200

    premio-cbn(atualizacoes)

---
+++ index.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>OPOVO CBN</title>
+  <meta charset="utf-8"/>
+  <meta lang="pt-br"/>
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="css/premiocbn.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+</head>
+<body>
+
+  <!-- menu -->
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="navbar px-lg-5 px-3 d-flex align-items-center justify-content-between">
+          <h1 class="float-l">
+            <a href="#"><img src="img/logo-cbn.png" style="width: 120px;"></a>
+          </h1>
+
+          <input type="checkbox" id="control-nav" />
+          <label for="control-nav" class="control-nav"></label>
+          <label for="control-nav" class="control-nav-close"></label>
+
+          <nav class="float-r" id="nav">
+            <ul class="list-auto">
+              <li>
+                <a href="#sobre" id="sobre-link">Sobre</a>
+              </li>
+              <li>
+                <a href="#calendario" id="calendario-link">Calendário</a>
+              </li>
+              <li>
+                <a href="#juri" id="juri-link">Juri Técnico</a>
+              </li>
+              <li>
+                <a href="#inscricao" id="inscricao-link">Inscrição</a>
+              </li>
+              <li>
+                <a href="#premiacao" id="premiacao-link">Premiação</a>
+              </li>
+              <li>
+                <a href="#patrocinadores" id="patrocinadores-link">Patrocinadores</a>
+              </li>
+              <li>
+                <a href="#faq">FAQ</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- header -->
+
+  <header class="topo-menu d-flex flex-column align-items-center justify-content-center py-5">
+    <img src="img/logo-topo.png" class="logo-topo">
+
+    <div class="content-logo"></div>
+
+    <h1 style="text-align: center;color: white;" class="mb-5">1° PRÊMIO DO O POVO CBN DE <br/>RADIOJORNALISMO UNIVERSITÁRIO</h1>
+    <button class="button">INSCREVA-SE</button>
+  </header>
+
+  <!-- 1° sessão -->
+
+  <section class="container" id="sobre">
+
+    <div class="row">
+      <div class="col-lg-6 col-12">
+        <div class="titulo">
+          <h1>O Prêmio</h1>
+        </div>
+      </div>
+      <div class="col-lg-6 col-12">
+        <div class="">
+          <p class="textopremio">O Prêmio O POVO CBN, que realiza neste ano sua primeira edição, tem como objetivo aproximar os futuros profissionais ao mundo do radiojornalismo, fortalecendo os laços entre mercado e universidade, além de valorizar o rico trabalho desenvolvido pelos estudantes da área. O tema escolhido para essa estreia é "Preservação do Meio Ambiente e Desenvolvimento Sustentável no Ceará". Por meio de uma reportagem de até 3 minutos, estudantes de Jornalismo de todo o estado poderão abordar essa questão sob o ponto de vista que identificarem como mais relevante. Os trabalhos podem ser em grupo ou individuais.</p>
+
+          <div class="content-inscricoes">
+            <p class="textopremio"><b>As inscrições irão até o dia 20 de janeiro de 2020</b></p>
+            <p class="textopremio mb-1">Mais informações no edital do Prêmio. <b>Participe!</b></p>
+          </div>
+
+          <p class="bt-edital mt-1"><a href="#">Clique aqui</a> e leia o edital completo</p>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+  <!-- 2° sessão -->
+
+  <section class="container-fluid py-0 calendario" id="calendario">
+
+    <div class="row">
+
+      <div class="col-12 px-0 caixa-cinza">
+        <div class="caixa-vermelha d-flex align-items-center">
+          <div class="wow fadeInRight">
+            <div class="titulo titulo-branco">
+              <h1>Calendário</h1>
+            </div>
+
+
+            <ul class="listacalendario py-0">
+              <li>
+                <h3>PERÍODO DO PRÊMIO:</h3>
+                <p>0X/0X/2019 a 0X/0X/2020</p>
+              </li>
+
+              <li>
+                <h3>INSCRIÇÕES E UPLOAD</h3>
+                <p>Das Xh* do dia X de Janeiro até às Xh* do dia X de Janeiro. Marque na agenda para não perder a data e a hora das inscrições.</p>
+              </li>
+
+              <li>
+                <h3>AVALIAÇÃO DO JÚRI</h3>
+                <p>Das Xh* do dia 0X de Janeiro até às Xh* do dia 0X de Janeiro. Se você não conquistou sua vaga na final pelo voto popular, ainda há uma chance. Os três primeiros colocados de 2020 vão votar nos 20 melhores projetos, estes se juntarão aos 30 da etapa anterior.</p>
+              </li>
+              <li>
+               <h3>DIVULGAÇÃO DOS 50 FINALISTAS</h3>
+               <p>Dia 0X de janeiro às Xh*. Neste momento vamos conhecer os concorrentes que irão para a avaliação final realizada pelo júri técnico.</p>
+             </li>
+
+             <li>
+              <h3>AVALIAÇÃO DO JÚRI TÉCNICO</h3>
+              <p>Das Xh* do dia 0X de janeiro até às Xh* do dia 0X de Janeiro. Vai ser a hora de torcer e esperar.</p>
+            </li>
+
+            <li>
+              <h3>DIVULGAÇÃO DO RESULTADO</h3>
+              <p>Às Xh* do dia 0X de Janeiro. É o dia que a espera acaba e você vai conferir se está entre os 3 vencedores. 
+              * Horário de Brasília.</p>
+            </li>
+            <li>
+                <h3>PERÍODO DO PRÊMIO:</h3>
+                <p>0X/0X/2019 a 0X/0X/2020</p>
+              </li>
+
+              <li>
+                <h3>INSCRIÇÕES E UPLOAD</h3>
+                <p>Das Xh* do dia X de Janeiro até às Xh* do dia X de Janeiro. Marque na agenda para não perder a data e a hora das inscrições.</p>
+              </li>
+
+              <li>
+                <h3>AVALIAÇÃO DO JÚRI</h3>
+                <p>Das Xh* do dia 0X de Janeiro até às Xh* do dia 0X de Janeiro. Se você não conquistou sua vaga na final pelo voto popular, ainda há uma chance. Os três primeiros colocados de 2020 vão votar nos 20 melhores projetos, estes se juntarão aos 30 da etapa anterior.</p>
+              </li>
+              <li>
+               <h3>DIVULGAÇÃO DOS 50 FINALISTAS</h3>
+               <p>Dia 0X de janeiro às Xh*. Neste momento vamos conhecer os concorrentes que irão para a avaliação final realizada pelo júri técnico.</p>
+             </li>
+
+             <li>
+              <h3>AVALIAÇÃO DO JÚRI TÉCNICO</h3>
+              <p>Das Xh* do dia 0X de janeiro até às Xh* do dia 0X de Janeiro. Vai ser a hora de torcer e esperar.</p>
+            </li>
+
+            <li>
+              <h3>DIVULGAÇÃO DO RESULTADO</h3>
+              <p>Às Xh* do dia 0X de Janeiro. É o dia que a espera acaba e você vai conferir se está entre os 3 vencedores. 
+              * Horário de Brasília.</p>
+            </li>
+          </ul>
+
+          <br>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</section>
+
+
+<!-- 3° sessão -->
+
+<section class="container juri" id="juri">
+
+  <div class="row">
+    <div class="col-lg-4 col-12">
+      <div class="titulo especifico">
+        <h1>Juri Técnico</h1>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-12 mt-lg-0">
+      <figure class="img-juri">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0u4KYAIsfaAID1BlzTTZcrkag88wrOKBzV2jpF1E7RzZ2MkHp" alt="" />
+      </figure>
+      <h3>Nome do Juri 1</h3>
+      <p>
+        Pintor muralista, começou adolescente produzindo graffitis. Dono de um estilo impactante, inspirado na xilogravura, tem trabalhos expostos em cidades como Amsterdam e Brasil
+      </p>
+    </div>
+
+    <div class="col-lg-4 col-12 mt-lg-0 mt-4">
+      <figure class="img-juri">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0u4KYAIsfaAID1BlzTTZcrkag88wrOKBzV2jpF1E7RzZ2MkHp" alt="" />
+      </figure>
+      <h3>Nome do Juri 2</h3>
+      <p>
+        Pintor muralista, começou adolescente produzindo graffitis. Dono de um estilo impactante, inspirado na xilogravura, tem trabalhos expostos em cidades como Amsterdam e Brasil
+      </p>
+    </div>
+  </div>
+
+  <div class="row mt-lg-5 mt-4">
+    <div class="col-lg-4 col-12">
+      <figure class="img-juri">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0u4KYAIsfaAID1BlzTTZcrkag88wrOKBzV2jpF1E7RzZ2MkHp" alt="" />
+      </figure>
+      <h3>Nome do Juri 3</h3>
+      <p>
+        Pintor muralista, começou adolescente produzindo graffitis. Dono de um estilo impactante, inspirado na xilogravura, tem trabalhos expostos em cidades como Amsterdam e Brasil
+      </p>
+    </div>
+    <div class="col-lg-4 col-12 mt-lg-0 mt-4">
+      <figure class="img-juri">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0u4KYAIsfaAID1BlzTTZcrkag88wrOKBzV2jpF1E7RzZ2MkHp" alt="" />
+      </figure>
+      <h3>Nome do Juri 4</h3>
+      <p>
+        Pintor muralista, começou adolescente produzindo graffitis. Dono de um estilo impactante, inspirado na xilogravura, tem trabalhos expostos em cidades como Amsterdam e Brasil
+      </p>
+    </div>
+    <div class="col-lg-4 col-12 mt-lg-0 mt-4">
+      <figure class="img-juri">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0u4KYAIsfaAID1BlzTTZcrkag88wrOKBzV2jpF1E7RzZ2MkHp" alt="" />
+      </figure>
+      <h3>Nome do Juri 5</h3>
+      <p>
+        Pintor muralista, começou adolescente produzindo graffitis. Dono de um estilo impactante, inspirado na xilogravura, tem trabalhos expostos em cidades como Amsterdam e Brasil
+      </p>
+    </div>
+  </div>
+
+</section>
+
+
+<!-- 4° e 5° sessão -->
+
+<section class="container-fluid imageminscricao" id="inscricao">
+  <div class="container">
+    <div class="row">
+      <div class="col col-lg-6 col-12">
+        <div class="titulo especifico">
+          <h1>Inscreva seu Projeto</h1>
+        </div>
+      </div>
+      <div class="col col-lg-6 col-12 d-flex align-items-center px-4 flex-lg-row flex-column">
+        <div class="forminscricoes">
+
+          <form action="http://192.168.75.31:8000/newRegistration" type="POST">
+            <label for="fname">Nome Completo*</label>
+            <input type="text" name="name" required="true">
+
+            <!-- CAMPOS DUPLOS -->
+            <div class="d-flex justify-content-between flex-lg-row flex-column">
+
+              <div class="metade">
+
+                <label for="lname">CPF*</label>
+                <input type="text" name="cpf" required="true">
+
+              </div>
+              <div class="metade">
+
+                <label for="lname">E-mail*</label>
+                <input type="text" name="email" required="true">
+
+              </div>
+
+            </div>
+
+            <label for="lname">Estudantes que também assinam a reportagem</label>
+            <input type="text" name="other_students">
+
+            <!-- CAMPOS DUPLOS -->
+            <div class="d-flex justify-content-between flex-lg-row flex-column">
+
+              <div class="metade">
+
+                <label for="lname">Título da matéria inscrita*</label>
+                <input type="text" name="title" required="true">
+
+              </div>
+              <div class="metade">
+
+                <label for="lname">Telefone*</label>
+                <input type="text" name="phone" required="true">
+
+              </div>
+
+            </div>
+
+            <label for="lname">Endereço para contato*</label>
+            <input type="text" name="address" required="true">
+
+            <label for="lname">Instituição de Ensino</label>
+            <input type="text" name="institute">
+
+            <p class="bt-edital mt-1"><input type="file" name="" value="Upload da Reportagem" required="true"> (*áudio de até 3min no formato MP3 128kbps)</p>
+
+            <div class="checkbox d-flex ">
+              <input type="checkbox" name="" required="true"><span>Concordo com os termos do edital</span>
+            </div>
+            <button class="button mt-3"><b>ENVIAR</b></button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<section class="container-fluid" style="background-color: #B12A31;" id="premiacao">
+
+  <div class="row">
+    <div class="col-sm">
+      <div class="titulo titulo-branco">
+        <h1>Premiação</h1>
+      </div>
+    </div>
+    <div class="col-sm d-flex align-items-center">
+      <div class="textopremiacao">
+        <p>O grande vencedor receberá um prêmio de xx.xxx.xxx. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <div class="content">
+          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+      </div>
+    </div>
+    
+  </section>
+  
+
+  <!-- 6°sessão -->
+
+  <section class="container" id="patrocinadores">
+
+    <div class="titulo">
+      <h1>Patrocinadores</h1>
+    </div>
+    <br>
+
+    <div class="row px-lg-5 m-auto d-flex align-items-center justify-content-center">
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+    </div>
+
+    <div class="row px-lg-5 m-auto d-flex align-items-center justify-content-center">
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+      <div class="col-lg-3 col-12 py-2">
+        <img src="http://www.flexpe.com.br/wp-content/uploads/2014/07/burgundy.png" style="width: 100%;height: 50px;">
+      </div>
+    </div>
+
+  </section>
+
+  <br/>
+
+
+  <footer class="footer px-lg-5 px-3 d-flex align-items-center">
+    <a href="#">
+      <img src="img/logo-cbn.png" style="width: 100px;margin-top: 15px;">
+    </a>
+    <span class="pl-3 ml-3 d-lg-block d-none">Copyright 1997-2018</span>
+  </footer>
+
+
+</body>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.scrolly.min.js"></script>
+<script src="js/jquery.scrollex.min.js"></script>
+<script src="js/browser.min.js"></script>
+<script src="js/breakpoints.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/wow.min.js"></script>
+<script>
+  new WOW().init();
+</script>
+</html>
